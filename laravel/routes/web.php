1952d21388faf89c633ce6d91b328dac835bd537
kishormahmud9 <topfazil999@gmail.com>
2024-02-20 11:58:08 +0600

    kishor:update profile photo and name

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\HomeSettingsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -37,6 +38,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+Route::middleware('auth','role:admin')->group(function () {
+    // home settings related routes 
+    Route::controller(HomeSettingsController::class)->group(function () {
+        Route::get('/home-setting', 'index')->name('home.settings');
+    });
 });
 
 //Admin login forgot Pw Route
